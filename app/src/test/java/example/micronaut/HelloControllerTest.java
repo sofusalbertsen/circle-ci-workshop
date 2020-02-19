dda539409f8e9c9f356efac022462ae86887c105
Sofus Albertsen <soa@praqma.com>
2020-02-19 18:57:10 +0100

    made test better

--- app/src/test/java/example/micronaut/HelloControllerTest.java
+++ app/src/test/java/example/micronaut/HelloControllerTest.java
@@ -25,7 +25,7 @@ public class HelloControllerTest {
         String body = client.toBlocking().retrieve(request);
 
         assertNotNull(body);
-        assertEquals("Hello sofus", body);
+        assertEquals("Hello Sofus", body);
     }
     @Test
     public void testCombineName() {
